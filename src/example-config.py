ef1d3e3f0b5cf25bdacaf16fdbf1eccc028ca1f6
Sebastian Beschke <s.beschke@gmx.de>
2011-06-24 11:47:26 +0200

    Removed everything platform-dependent from SConstruct

--- src/example-config.py
+++ src/example-config.py
@@ -1,3 +1,9 @@
 CLANLIB_INCLUDE = "/usr/include/ClanLib-2.2"
 #CLANLIB_LIBPATH = "/home/sebastian/projects/orx-1.3rc0/dew-linux/lib"
 
+def setupEnvironment(env):
+	env['CPPPATH'] = CLANLIB_INCLUDE
+	env['LIBS'] = 'pthread'
+	env.ParseConfig("pkg-config clanCore-2.2 clanApp-2.2 clanDisplay-2.2 clanGL-2.2 --cflags --libs")
+
+
