73eb1396c6d1eafe07141d2159a9b8b07ee9e574
Waynix <waynix@blacky.(none)>
2011-06-25 12:14:45 +0200

    Revert "OnScreenMessage removed"
    This reverts commit 05bdc638193b443a6ef45de659a5aaf8951f3598.

---
+++ src/OnScreenMessage.h
@@ -0,0 +1,42 @@
+/*
+ * OnScreenMessage.h
+ *
+ *  Created on: 25.06.2011
+ *      Author: sebastian
+ */
+
+#ifndef ONSCREENMESSAGE_H_
+#define ONSCREENMESSAGE_H_
+
+#include <string>
+#include <ClanLib/core.h>
+#include <ClanLib/display.h>
+#include <ClanLib/gl.h>
+
+class OnScreenMessage {
+public:
+	OnScreenMessage() {}
+	OnScreenMessage(CL_Pointf textPos, CL_FontDescription font, CL_Colorf color);
+	~OnScreenMessage();
+
+	void setMessage(const std::string message);
+	void setMessage(const std::string message, float timeout);
+
+	void tick(float secs);
+	void draw(void);
+	void hide(void);
+
+private:
+	bool show;
+	std::string message;
+	bool hasTimeout;
+	float timeout;
+
+	CL_Pointf textPos;
+
+	CL_Colorf color;
+	CL_Font_System fontSystem;
+
+};
+
+#endif /* ONSCREENMESSAGE_H_ */
