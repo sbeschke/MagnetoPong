75e4e455b01b5eee75413b08db4dfffa1af66b60
Sebastian Beschke <s.beschke@gmx.de>
2011-06-25 03:39:35 +0200

    example-config

--- src/example-config.py
+++ src/example-config.py
@@ -1,9 +1,10 @@
 CLANLIB_INCLUDE = "/usr/include/ClanLib-2.2"
 #CLANLIB_LIBPATH = "/home/sebastian/projects/orx-1.3rc0/dew-linux/lib"
+OPENNI_INCLUDE = "/home/sebastian/projects/openni/OpenNI/Include"
 
 def setupEnvironment(env):
-	env['CPPPATH'] = CLANLIB_INCLUDE
-	env['LIBS'] = 'pthread'
+	env['CPPPATH'] = [CLANLIB_INCLUDE, OPENNI_INCLUDE]
+	env['LIBS'] = ['pthread', 'OpenNI']
 	env.ParseConfig("pkg-config clanCore-2.2 clanApp-2.2 clanDisplay-2.2 clanGL-2.2 --cflags --libs")
 
 
